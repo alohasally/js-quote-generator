c72539a196fccc26580d948cd8e7c9273913bae9
Sally <peacheeks@gmail.com>
2022-07-22 11:51:19 +0900

    third

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuotesBtn = document.getElementById('new-quote')
+
+
 let apiQuotes = [];
 
 //Show New Quote
 function newQuotes(){
     // Pick a random  quote from apiquotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
-
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
 async function getQuotes(){
-    const apiUrl = 'https://type.fit/api/qguotes';
+    const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
@@ -23,5 +30,3 @@ async function getQuotes(){
 
 //On Load
 getQuotes();
-
-//newQuotes();
